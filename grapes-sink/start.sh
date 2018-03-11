2cd78c902bc441562ec53087c6bb28c287079212
Salman Malik <smalik@pivotal.io>
2018-03-10 19:33:44 -0500

    the consumer groups names can now be overridden

--- grapes-sink/start.sh
+++ grapes-sink/start.sh
@@ -5,7 +5,8 @@ while read -r f1 f2 PORT HOST; do
   KAFKA_BROKERS="${HOST%?}:${PORT},${KAFKA_BROKERS}"
 done < <(dig SRV $KAFKA_HEADLESS_SVC +short)
 
-java -Djava.security.egd=file:/dev/./urandom \
-  -jar boot-app.jar \
+java -jar boot-app.jar \
   --spring.cloud.stream.kafka.binder.brokers="${KAFKA_BROKERS%?}" \
-  --spring.cloud.stream.kafka.binder.zkNodes="zk-cs:${ZK_CS_SERVICE_PORT}"
+  --spring.cloud.stream.kafka.binder.zkNodes="zk-cs:${ZK_CS_SERVICE_PORT}" \
+  --spring.cloud.stream.bindings.single-grape.group="${GRAPES_CONSUMER_GROUP:-amqp-sink}" \
+  --spring.cloud.stream.bindings.multi-grapes.group="${GRAPES_CONSUMER_GROUP:-amqp-sink}"
--- grapes-source/start.sh
+++ grapes-source/start.sh
@@ -5,7 +5,6 @@ while read -r f1 f2 PORT HOST; do
   KAFKA_BROKERS="${HOST%?}:${PORT},${KAFKA_BROKERS}"
 done < <(dig SRV $KAFKA_HEADLESS_SVC +short)
 
-java -Djava.security.egd=file:/dev/./urandom \
-  -jar boot-app.jar \
+java -jar boot-app.jar \
   --spring.cloud.stream.kafka.binder.brokers="${KAFKA_BROKERS%?}" \
   --spring.cloud.stream.kafka.binder.zkNodes="zk-cs:${ZK_CS_SERVICE_PORT}"
