6a7fd09d97c340f52471321e328b51b6fb0b444e
Salman Malik <smalik@pivotal.io>
2018-05-17 16:57:46 -0400

    Updating startup scripts for running in docker containers with kafka profile

--- grapes-sink/start.sh
+++ grapes-sink/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 
-KAFKA_HEADLESS_SVC="_broker._tcp.kafka-hs.${NAMESPACE}.svc.cluster.local"
+KAFKA_HEADLESS_SVC="_broker._tcp.kafka.${NAMESPACE}.svc.cluster.local"
 while read -r f1 f2 PORT HOST; do
   KAFKA_BROKERS="${HOST%?}:${PORT},${KAFKA_BROKERS}"
 done < <(dig SRV $KAFKA_HEADLESS_SVC +short)
 
 java -jar boot-app.jar \
+  --spring.profiles.active=kafka \
   --spring.cloud.stream.kafka.binder.brokers="${KAFKA_BROKERS%?}" \
-  --spring.cloud.stream.kafka.binder.zkNodes="zk-cs:${ZK_CS_SERVICE_PORT}" \
+  --spring.cloud.stream.kafka.binder.zkNodes="zk:${ZK_SERVICE_PORT}" \
   --spring.cloud.stream.bindings.single-grape.group="${GRAPES_CONSUMER_GROUP:-amqp-sink}" \
   --spring.cloud.stream.bindings.multi-grapes.group="${GRAPES_CONSUMER_GROUP:-amqp-sink}"
--- grapes-source/start.sh
+++ grapes-source/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -x
 
-KAFKA_HEADLESS_SVC="_broker._tcp.kafka-hs.${NAMESPACE}.svc.cluster.local"
+KAFKA_HEADLESS_SVC="_broker._tcp.kafka.${NAMESPACE}.svc.cluster.local"
 while read -r f1 f2 PORT HOST; do
   KAFKA_BROKERS="${HOST%?}:${PORT},${KAFKA_BROKERS}"
 done < <(dig SRV $KAFKA_HEADLESS_SVC +short)
 
 java -jar boot-app.jar \
+  --spring.profiles.active=kafka \
   --spring.cloud.stream.kafka.binder.brokers="${KAFKA_BROKERS%?}" \
-  --spring.cloud.stream.kafka.binder.zkNodes="zk-cs:${ZK_CS_SERVICE_PORT}"
+  --spring.cloud.stream.kafka.binder.zkNodes="zk:${ZK_SERVICE_PORT}"
